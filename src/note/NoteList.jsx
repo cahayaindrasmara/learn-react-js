2f60d2052808d7d4805d84b02c8dc4a89506b5a2
cahayaindrasmara <indrasmara.cahaya27@gmail.com>
2025-11-12 13:10:13 +0700

    learn memo

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,6 +1,7 @@
-import { useContext } from "react"
+import { useContext, useMemo, useRef, useState } from "react"
 import Note from "./Note"
 import { NotesContext } from "./NoteContect"
+// import { NotesContext } from "./NoteContect"
 
 // export default function NoteList({notes, onChange, onDelete}) {
 //     return (
@@ -13,13 +14,45 @@ import { NotesContext } from "./NoteContect"
 // }
 
 //menggunakan context
+// export default function NoteList() {
+//     const notes = useContext(NotesContext)
+//     return (
+//         <ul>
+//             {notes.map(note => (
+//                 <li key={note.id}><Note note={note}/></li>
+//             ))}
+//         </ul>
+//     )
+// }
+
+//menggunakan memo
 export default function NoteList() {
-    const notes = useContext(NotesContext)
+    const notes = useContext(NotesContext);
+    const [search, setSearch] = useState("");
+    const searchInput = useRef(null);
+
+    const filteredNotes = useMemo(() => {
+        console.info("Filtering Notes")
+        return notes.filter(note => note.text.includes(search));
+    }, [notes, search])
+
+    function handleSearch() {
+        console.info("Seacrh");
+        setSearch(searchInput.current.value)
+    }
+
     return (
-        <ul>
-            {notes.map(note => (
-                <li key={note.id}><Note note={note}/></li>
-            ))}
-        </ul>
+        <div>
+            <input type="text" placeholder="Search" ref={searchInput} />
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+                {filteredNotes.map(note => (
+                    <li key={note.id}>
+                        <Note note={note}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
+
